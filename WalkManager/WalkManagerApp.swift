cb1853dad2c21a1ffab4d171a3f0dc5ca7c3b7ea
Smaran Vallabhaneni <smaranvallabhaneni@gmail.com>
2026-08-02 13:03:42 +0530

    Initial Commit

---
+++ WalkManager/WalkManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkManagerApp.swift
+//  WalkManager
+//
+//  Created by Smaran Vallabhaneni on 02/08/26.
+//
+
+import SwiftUI
+
+@main
+struct WalkManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
